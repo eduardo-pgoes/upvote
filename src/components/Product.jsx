4d6f9911cf76da4626ea0369aca1d282e7e6270e
Eduardo Goes <e.goes@unifesp.br>
2022-09-16 17:41:07 -0300

    ✨ feature: add arrows to product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
+import { FiArrowUp, FiArrowDown } from 'react-icons/fi/'
 
 class Product extends React.Component {
     render() {
         return (
-            <div class="flex w-screen bg-green-800 item p-8 mx-auto rounded-md shadow-sm">
-                <div class="image">
+            <div class="flex w-screen bg-green-800 item p-6 mx-auto rounded-md text-white shadow-sm">
+                <div class="image flex-initial justify-center self-center">
                     <img alt="foto aleatória" src="https://ui-avatars.com/api/?background=random"/>
                 </div>
-                <div class="description flex items-start justify-center pl-6 pt-px flex-col flex-auto text-white">
+                <div class="description flex items-start justify-center pl-6 pt-px flex-col flex-auto">
                     <h1 class="font-bold">{this.props.title}</h1>
                     <p>{this.props.description}</p>
+                </div>
+                <div class="p-4 space-y-2 mx-auto">
+                    <FiArrowUp/>
+                    <FiArrowDown/>
                 </div>
             </div>
         );
