224ab40d38d315ff5a12fb7b173d45e52fa6d077
Eduardo Goes <e.goes@unifesp.br>
2022-09-19 21:19:08 -0300

    ✨ sort products by number of votes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,10 @@ import Seed from '../data/seed';
 
 class ProductList extends React.Component {
     render() {
-        const productComponents = Seed().products.map((product => (
+        const products = Seed().products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+        const productComponents = products.map((product => (
                 <Product
                     id={product.id}
                     title={product.title}
